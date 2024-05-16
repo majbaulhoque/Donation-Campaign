32ace9f7cb6583a71afd58a2af5cb856f3e6bdc5
majbaulhoque <majba9399@gmail.com>
2024-05-17 02:11:35 +0600

    Added no found data use state through in Donation Card

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Donation = () => {
+    const [donation, setDonation] = useState([]);
+
+    const [noFound, setNoFound] = useState('')
+
+    useEffect(()=>{
+        const donationCards = JSON.parse(localStorage.getItem('donations'));
+        if (donationCards) {
+            setDonation(donationCards)
+        }else{
+            setNoFound("No Data Found !!!")
+        }
+    },[])
     return (
         <div>
-            <h3>This is Donation Page</h3>
+            {
+                noFound ? <p className="h-[60vh] flex items-center text-center justify-center text-3xl font-bold text-semibold">{noFound}</p> : <div></div>
+            }
         </div>
     );
 };
