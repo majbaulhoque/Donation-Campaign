b2cb45a6ed4142a605942642d534fcb60e66a889
majbaulhoque <majba9399@gmail.com>
2024-05-17 16:58:26 +0600

    show more & show less button added Dynamicaly useState through

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import DonationItemCard from "../Donation Item/DonationItemCard";
+
 import DonationCart from "./DonationCart";
 
 const Donation = () => {
     const [donation, setDonation] = useState([]);
 
-    const [noFound, setNoFound] = useState('')
+    const [noFound, setNoFound] = useState(false)
+    const [isShow, setIsShow] = useState(false)
 
     useEffect(() => {
         const donationCards = JSON.parse(localStorage.getItem('donations'));
@@ -17,27 +18,36 @@ const Donation = () => {
         }
     }, [])
 
-    const handleRemove = () =>{
+    const handleRemove = () => {
         localStorage.clear();
         setDonation([]),
-        setNoFound("No Data Found !!!")
+            setNoFound("No Data Found !!!")
     }
+    console.log(isShow)
     return (
         <div>
             {
                 noFound ? <p className="h-[60vh] flex items-center text-center justify-center text-3xl font-bold text-semibold">{noFound}</p> :
 
-                <div>
-                    {
-                        donation.length > 0 && <button onClick={handleRemove} className="bg-blue-600 text-white px-4 py-1 my-5 block mx-auto">Delete ALL</button>
-                    }
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                    <div>
                         {
-                            donation?.map(donation => <DonationCart key={donation.id} donation={donation}></DonationCart>)
+                            donation.length > 0 && <button onClick={handleRemove} className="bg-blue-600 text-white px-4 py-1 my-5 block mx-auto">Delete ALL</button>
                         }
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                            {
+                                isShow ? donation?.map(donation => <DonationCart key={donation.id} donation={donation}></DonationCart>)
+                                :
+                                donation?.slice(0,4).map(donation => <DonationCart key={donation.id} donation={donation}></DonationCart>)
+                            }
+                        </div>
                     </div>
-                </div>
+
             }
+            <div>
+                {
+                    donation.length > 4 && <button onClick={() => setIsShow(!isShow)} className="bg-green-600 text-white px-4 py-1 my-5 block mx-auto">{isShow ? 'See Less' : 'See More'}</button>
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Donation/DonationCart.jsx
+++ src/Pages/Donation/DonationCart.jsx
@@ -1,3 +1,4 @@
+
 import { useNavigate } from "react-router-dom";
 
 const DonationCart = ({ donation }) => {
@@ -7,19 +8,21 @@ const DonationCart = ({ donation }) => {
     const handleViewDetails = () => {
         navigate('/viewDetails')
     }
+
     return (
         <div>
-            <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl ">
-                <img className="w-full rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-s-lg lg:h-36" src={picture} alt="" />
-                <div className="flex flex-col justify-between p-4 leading-normal">
-                    <p>{title}</p>
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight ">{category}</h5>
-                    <p>{price}</p>
+            <div>
+                <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl ">
+                    <img className="w-full rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-s-lg lg:h-36" src={picture} alt="" />
+                    <div className="flex flex-col justify-between p-4 leading-normal">
+                        <p>{title}</p>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight ">{category}</h5>
+                        <p>{price}</p>
 
-                    <button onClick={handleViewDetails}>View Details</button>
-                </div>
-            </a>
-
+                        <button onClick={handleViewDetails}>View Details</button>
+                    </div>
+                </a>
+            </div>
         </div>
     );
 };
